ec0b09a7a4b9a9c1bf18d5d82a1c7a01b65d1368
Nazar.M <inazar.m@gmail.com>
2020-07-18 20:38:07 +0500

    Update HomeController.cs
    CreateStudent Action added in Home Controller

--- CrudWithAjax/Controllers/HomeController.cs
+++ CrudWithAjax/Controllers/HomeController.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CrudWithAjax.Models;
 
 namespace CrudWithAjax.Controllers
 {
     public class HomeController : Controller
     {
+        exampleEntities db = new exampleEntities();
         public ActionResult Index()
         {
             return View();
         }
-
+        
+        public ActionResult CreateStudent(tblTask std)
+        {
+            db.tblTasks.Add(std);
+            db.SaveChanges();
+            string msg = "Success";
+            return Json(new { Message = msg, JsonRequestBehavior.AllowGet });
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
